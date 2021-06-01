0890596f649c0c9921a867b824e1b70e2c981068
marlon santana <marlonnig70@@gmail.com>
2021-05-31 22:41:08 -0300

    integrando pagamento no stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,5 +1,7 @@
-import styles from './styles.module.scss';
-
+import { useSession, signIn } from "next-auth/client";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
+import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
     priceId: string;
@@ -8,12 +10,41 @@ interface SubscribeButtonProps {
 
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+    const [session] = useSession();
+    
+
+    async function handleSubscribe() {
+        if (!session) {
+            signIn('github')
+            return
+        }
+
+        try {
+            const response =  await api.post('/subscribe')
+            console.log('primeiro')
+
+            const { sessionId } = response.data
+             console.log('2')
+            
+             const stripe = await getStripeJs()
+              console.log('3')
+
+            await stripe.redirectToCheckout({sessionId})
+             console.log('4')
+             
+        } catch (err) {
+            alert(err.message);
+        }
+        
+    }
+    
     return (
         <button 
         type="button"
         className={styles.subscribeButton}
+        onClick={handleSubscribe}
         > 
         Subscribe Now
         </button>
-    );
+    )
 }
