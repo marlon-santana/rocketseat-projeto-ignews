0a41a73ad9ad9ef8a581c983668dcaa485f78ebb
marlon santana <marlonnig70@gmail.com>
2021-06-10 06:40:55 -0300

    listando posts em tela

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,9 +1,26 @@
 import Head from 'next/head';
+import { GetStaticProps } from 'next';
+
+import { getPrismicClient } from '../../services/prismic';
+import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
+
 import styles from './styles.module.scss';
 
 
+type Post = {
+    slug: string;
+    title: string;
+    excerpt: string;
+    updatedAt: string;
+};
 
-export default function Posts() {
+interface PostProps {
+    posts: Post[]
+}
+
+
+export default function Posts( { posts }: PostProps ) {
     return (
         <>
             <Head>
@@ -12,25 +29,45 @@ export default function Posts() {
 
             <main className={styles.container}>
                 <div className={styles.posts}>
-                    <a href="#">
-                    <time>12 de março de 2021</time>
-                    <strong>Creating a Monorepo wuith lerna e yarn workspace</strong>
-                    <p>Nesse guia você vai aprender a criar um monorepo para gerenciar multiplos pacotes com ...</p>
+                { posts.map(post => (
+                    <a key={post.slug} href="#">
+                    <time>{post.updatedAt}</time>
+                    <strong>{post.title}</strong>
+                    <p>{post.excerpt}</p>
                     </a>
-
-                    <a href="#">
-                    <time>12 de março de 2021</time>
-                    <strong>Creating a Monorepo wuith lerna e yarn workspace</strong>
-                    <p>Nesse guia você vai aprender a criar um monorepo para gerenciar multiplos pacotes com ...</p>
-                    </a>
-
-                    <a> href="#"
-                    <time>12 de março de 2021</time>
-                    <strong>Creating a Monorepo wuith lerna e yarn workspace</strong>
-                    <p>Nesse guia você vai aprender a criar um monorepo para gerenciar multiplos pacotes com ...</p>
-                    </a>
+                    ))}
                 </div>
             </main>
         </>
     );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const prismic = getPrismicClient()
+
+    const response = await prismic.query([
+        Prismic.predicates.at('document.type','publication')
+    ], {
+        fetch: ['publication.title', 'publication.content'],
+        pageSize:100,
+    })
+    const posts  = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type === 'paragraph') ?.text ?? '' ,
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+                day:'2-digit',
+                month:'long',
+                year:'numeric'
+            })
+        };
+    });
+   
+
+    return {
+        props: {
+            posts
+            }
+    }
 }
